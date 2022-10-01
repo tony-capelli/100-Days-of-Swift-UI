cd54c0ddfe7732122d82ee4941804715be7dd3de
Tony Capelli <114808357+tony-capelli@users.noreply.github.com>
2022-10-01 14:51:26 +0200

    New Project
    New project: Bookworm

---
+++ Bookworm/Bookworm/DataController.swift
@@ -0,0 +1,21 @@
+//
+//  DataController.swift
+//  Bookworm
+//
+//  Created by Tony Capelli on 02/10/22.
+//
+
+import CoreData
+import Foundation
+
+class DataController: ObservableObject {
+    let container = NSPersistentContainer(name: "Bookworm")
+    
+    init() {
+        container.loadPersistentStores { description, error in
+            if let error = error {
+                print("Core Data failed to load: \(error.localizedDescription)")
+            }
+        }
+    }
+}
