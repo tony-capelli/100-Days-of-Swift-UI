d993662965dd1a2a69021b38144dacd37ab3d29c
Tony Capelli <114808357+tony-capelli@users.noreply.github.com>
2022-10-01 14:51:26 +0200

    New Project
    New project: Bookworm

---
+++ Bookworm/Bookworm/BookwormApp.swift
@@ -0,0 +1,17 @@
+//
+//  BookwormApp.swift
+//  Bookworm
+//
+//  Created by Tony Capelli on 01/10/22.
+//
+
+import SwiftUI
+
+@main
+struct BookwormApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
