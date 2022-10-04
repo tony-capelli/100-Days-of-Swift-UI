3b5ffd3d901d388f1a1f2f6a74647d1a894e8e97
Tony Capelli <114808357+tony-capelli@users.noreply.github.com>
2022-10-04 21:10:22 +0200

    Start of new Project

---
+++ InstaFilterApp/InstaFilterApp/ImagePicker.swift
@@ -0,0 +1,23 @@
+//
+//  ImagePicker.swift
+//  InstaFilterApp
+//
+//  Created by Tony Capelli on 04/10/22.
+//
+
+import PhotosUI
+import SwiftUI
+
+struct ImagePicker: UIViewControllerRepresentable {
+    func makeUIViewController(context: Context) -> PHPickerViewController {
+        var config = PHPickerConfiguration()
+        config.filter = .images
+        
+        let picker = PHPickerViewController(configuration: config)
+        return picker
+    }
+    
+    func updateUIViewController(_ uiViewController: PHPickerViewController, context: Context) {
+    
+    }
+}
---
+++ InstaFilterApp/InstaFilterApp/InstaFilterAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  InstaFilterAppApp.swift
+//  InstaFilterApp
+//
+//  Created by Tony Capelli on 04/10/22.
+//
+
+import SwiftUI
+
+@main
+struct InstaFilterAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
