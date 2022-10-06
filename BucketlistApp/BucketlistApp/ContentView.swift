56a41c8a5634ce8b355d90b490ef14ad30a0a4dc
Tony Capelli <114808357+tony-capelli@users.noreply.github.com>
2022-10-06 12:31:36 +0200

    Added Map and annotations

--- BucketlistApp/BucketlistApp/ContentView.swift
+++ BucketlistApp/BucketlistApp/ContentView.swift
@@ -6,16 +6,68 @@
 //
 
 import SwiftUI
+import MapKit
 
 struct ContentView: View {
+    @State private var mapCoordinate = MKCoordinateRegion(center: CLLocationCoordinate2D(latitude: 50, longitude: 10), span: MKCoordinateSpan(latitudeDelta: 25, longitudeDelta: 25))
+    
+    @State private var locations = [Location]()
+    
+    @State private var selectedPlace: Location?
+    
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundColor(.accentColor)
-            Text("Hello, world!")
+        ZStack {
+            Map(coordinateRegion: $mapCoordinate, annotationItems: locations){ location in
+                MapAnnotation(coordinate: location.coordinate) {
+                    VStack {
+                        Image(systemName: "star.circle")
+                            .resizable()
+                            .foregroundColor(.red)
+                            .frame(width: 44, height: 44)
+                            .background(.white)
+                            .clipShape(Circle())
+                        
+                        Text(location.name)
+                    }
+                    .onTapGesture {
+                        selectedPlace = location
+                    }
+                }
+            }
+            .ignoresSafeArea()
+            
+            Circle()
+                .fill(.blue)
+                .opacity((0.3))
+                .frame(width: 32, height: 32)
+            
+            VStack{
+                Spacer()
+                HStack{
+                    Spacer()
+                    Button{
+                        let newLocation = Location(id: UUID(), name: "New Location", description: "", latitude: mapCoordinate.center.latitude, longitude: mapCoordinate.center.longitude)
+                        locations.append(newLocation)
+                    } label: {
+                        Image(systemName: "plus")
+                    }
+                    .padding()
+                    .background()
+                    .font(.title)
+                    .foregroundColor(.blue)
+                    .clipShape(Circle())
+                    .padding(.trailing)
+                    .shadow(color: .black, radius: 10, x: 2, y: 2)
+                }
+            }
         }
-        .padding()
+        .sheet(item: $selectedPlace){ place in
+            EditView(location: place) { newLocation in
+                if let index = locations.firstIndex(of: place) {
+                    locations[index] = newLocation
+                }
+            }
+        }
     }
 }
 
