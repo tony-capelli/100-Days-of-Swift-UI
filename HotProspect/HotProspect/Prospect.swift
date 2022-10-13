70cf042437362a6733799d0d6c8cf573ef4db5c7
Tony Capelli <114808357+tony-capelli@users.noreply.github.com>
2022-10-13 22:06:21 +0200

    day 84

---
+++ HotProspect/HotProspect/Prospect.swift
@@ -0,0 +1,23 @@
+//
+//  Prospect.swift
+//  HotProspect
+//
+//  Created by Tony Capelli on 10/10/22.
+//
+
+import SwiftUI
+
+class Prospect: Identifiable, Codable {
+    var id = UUID()
+    var name = "Anonymous"
+    var emailAddress = ""
+    var isContacted = false
+}
+
+@MainActor class Prospects: ObservableObject {
+    @Published var people: [Prospect]
+    
+    init() {
+        people = []
+    }
+}
