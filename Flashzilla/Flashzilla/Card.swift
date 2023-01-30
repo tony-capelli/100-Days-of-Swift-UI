bb7ef8d9da1d13452ed44db7b35f58c5e26365de
Tony Capelli <114808357+tony-capelli@users.noreply.github.com>
2023-01-30 16:36:31 +0100

    Flashzilla accessibility and building UI

---
+++ Flashzilla/Flashzilla/Card.swift
@@ -0,0 +1,16 @@
+//
+//  Card.swift
+//  Flashzilla
+//
+//  Created by Tony Capelli on 30/01/23.
+//
+
+import Foundation
+
+
+struct Card {
+    let prompt: String
+    let answer: String
+    
+    static let example = Card(prompt: "Who played the 13th doctor in doctor who?", answer: "Jodie whittaker")
+}
--- Flashzilla/Flashzilla/ContentView.swift
+++ Flashzilla/Flashzilla/ContentView.swift
@@ -6,61 +6,62 @@
 //
 
 import SwiftUI
-import CoreHaptics
+
+extension View {
+    func stacked (at position: Int, in total: Int) -> some View {
+        let offset = Double(total - position)
+        return self.offset(x: 0, y: offset * 10)
+    }
+}
 
 struct ContentView: View {
-    @State private var engine: CHHapticEngine?
+    @Environment(\.accessibilityDifferentiateWithoutColor) var differentiateWithoutColor
+    @State private var cards = Array<Card>(repeating: Card.example, count: 10)
     
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundColor(.accentColor)
-            Text("Hello, world!")
+        ZStack{
+            Image("background")
+                .resizable()
+                .ignoresSafeArea()
+            
+            VStack{
+                ZStack{
+                    ForEach(0..<cards.count, id:\.self) { index in
+                        CardView(card: cards[index]){
+                            withAnimation {
+                                removeCard(at: index)
+                            }
+                        }
+                        .stacked(at: index, in: cards.count)
+                    }
+                }
+            }
+            
+            if differentiateWithoutColor {
+                VStack{
+                    Spacer()
+                    
+                    HStack{
+                        Image(systemName: "xmark.circle")
+                            .background(.black.opacity(0.7))
+                            .clipShape(Circle())
+                        Spacer()
+                        Image(systemName: "checkmark.circle")
+                            .background(.black.opacity(0.7))
+                            .clipShape(Circle())
+                    }
+                    .foregroundColor(.white)
+                    .font(.largeTitle)
+                    .padding()
+                }
+            }
         }
-        .onAppear(perform: prepareHaptics)
-        .onTapGesture(perform: complexHaptics)
-        .padding()
     }
     
-    func prepareHaptics() {
-        guard CHHapticEngine.capabilitiesForHardware().supportsHaptics else { return }
-        
-        do {
-            engine = try CHHapticEngine()
-            try engine?.start()
-        } catch {
-            print("Some error has occured \(error.localizedDescription)")
-        }
+    func removeCard(at index: Int){
+        cards.remove(at: index)
     }
-    
-    func complexHaptics() {
-        guard CHHapticEngine.capabilitiesForHardware().supportsHaptics else { return }
-        
-        var events = [CHHapticEvent]()
-        
-        for i in stride(from: 0, to: 1, by: 0.1){
-            let intensity = CHHapticEventParameter(parameterID: .hapticIntensity, value: Float(i))
-            let sharpness = CHHapticEventParameter(parameterID: .hapticSharpness, value: Float(i))
-            let event = CHHapticEvent(eventType: .hapticTransient, parameters: [intensity, sharpness], relativeTime: i)
-            events.append(event)
-        }
-        
-        for i in stride(from: 0, to: 1, by: 0.1){
-            let intensity = CHHapticEventParameter(parameterID: .hapticIntensity, value: Float(1 - i))
-            let sharpness = CHHapticEventParameter(parameterID: .hapticSharpness, value: Float(1 - i))
-            let event = CHHapticEvent(eventType: .hapticTransient, parameters: [intensity, sharpness], relativeTime: 1 + i)
-            events.append(event)
-        }
-        
-        do {
-            let pattern = try CHHapticPattern(events: events, parameters: [])
-            let player = try engine?.makePlayer(with: pattern)
-            try player?.start(atTime: 0)
-        } catch {
-            print(error.localizedDescription)
-        }
-    }
+   
 }
 
 struct ContentView_Previews: PreviewProvider {
