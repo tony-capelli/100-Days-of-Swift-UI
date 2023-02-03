e73feffe125a31034e78a186e5f14653610e1a31
Tony Capelli <114808357+tony-capelli@users.noreply.github.com>
2023-02-03 14:40:20 +0100

    challenge 94

--- LayoutAndGeometry/LayoutAndGeometry/GeoScrollView.swift
+++ LayoutAndGeometry/LayoutAndGeometry/GeoScrollView.swift
@@ -8,20 +8,23 @@
 import SwiftUI
 
 struct GeoScrollView: View {
-    let colors: [Color] = [.red, .green, .blue, .orange, .pink, .purple, .yellow]
+    let colors: [Color] = [.red, .green, .blue, .orange, .pink, ., .yellow]
     
     var body: some View {
         GeometryReader { fullView in
             ScrollView {
                 ForEach(0..<50){ index in
                     GeometryReader { geo in
+                        Text("\(geo.frame(in: .global).minY)")
                         Text("Row #\(index)")
-                            .font(.title)
+                            .font(.title)
                             .frame(maxWidth: .infinity)
                             .background(colors[index % 7])
                             .rotation3DEffect(.degrees(geo.frame(in: .global).minY - fullView.size.height / 2) / 5, axis: (x: 0, y: 1, z: 0))
+                            .opacity(geo.frame(in: .global).minX)
                     }
                     .frame(height: 40 )
+                    
                 }
             }
         }
