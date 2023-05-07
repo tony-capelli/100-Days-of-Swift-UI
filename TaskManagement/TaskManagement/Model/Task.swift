e61f85646c1c4e7887ad67b2f281ece5b7bdd821
Tony Capelli <114808357+tony-capelli@users.noreply.github.com>
2023-05-07 17:48:30 +0200

    new project

---
+++ TaskManagement/TaskManagement/Model/Task.swift
@@ -0,0 +1,8 @@
+//
+//  Task.swift
+//  TaskManagement
+//
+//  Created by Tony Capelli on 07/05/23.
+//
+
+import Foundation
---
+++ TaskManagement/TaskManagement/TaskManagementApp.swift
@@ -0,0 +1,17 @@
+//
+//  TaskManagementApp.swift
+//  TaskManagement
+//
+//  Created by Tony Capelli on 07/05/23.
+//
+
+import SwiftUI
+
+@main
+struct TaskManagementApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
