c78d2ab1ed67130d56c2ef53c25d0ff6e1976e17
Tony Capelli <114808357+tony-capelli@users.noreply.github.com>
2023-05-07 17:49:43 +0200

    ok

--- TaskManagement/TaskManagement/Model/Task.swift
+++ TaskManagement/TaskManagement/Model/Task.swift
@@ -5,4 +5,16 @@
 //  Created by Tony Capelli on 07/05/23.
 //
 
-import Foundation
+import SwiftUI
+
+struct Task: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct Task_Previews: PreviewProvider {
+    static var previews: some View {
+        Task()
+    }
+}
